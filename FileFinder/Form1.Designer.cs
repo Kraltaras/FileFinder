8ff517493f4b05da646607b8cd85570d83ca32ec
Kraltaras <kraltaras@gmail.com>
2020-02-25 02:00:35 +0300

    first commit

---
+++ FileFinder/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace FileFinder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textAddress = new System.Windows.Forms.TextBox();
+            this.textFileName = new System.Windows.Forms.TextBox();
+            this.textSymbols = new System.Windows.Forms.TextBox();
+            this.labelAddress = new System.Windows.Forms.Label();
+            this.labelFileName = new System.Windows.Forms.Label();
+            this.labelSymbols = new System.Windows.Forms.Label();
+            this.buttonChangeAddress = new System.Windows.Forms.Button();
+            this.folderAddress = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.labelFound = new System.Windows.Forms.Label();
+            this.labelCurrentFile = new System.Windows.Forms.Label();
+            this.treeViewFiles = new System.Windows.Forms.TreeView();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.buttonPause = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textAddress
+            // 
+            this.textAddress.Location = new System.Drawing.Point(140, 12);
+            this.textAddress.Name = "textAddress";
+            this.textAddress.ReadOnly = true;
+            this.textAddress.Size = new System.Drawing.Size(431, 20);
+            this.textAddress.TabIndex = 0;
+            this.textAddress.Text = "Desktop";
+            this.textAddress.TextChanged += new System.EventHandler(this.textAddress_TextChanged);
+            // 
+            // textFileName
+            // 
+            this.textFileName.Location = new System.Drawing.Point(140, 38);
+            this.textFileName.Name = "textFileName";
+            this.textFileName.Size = new System.Drawing.Size(431, 20);
+            this.textFileName.TabIndex = 1;
+            this.textFileName.TextChanged += new System.EventHandler(this.textFileName_TextChanged);
+            // 
+            // textSymbols
+            // 
+            this.textSymbols.Location = new System.Drawing.Point(140, 64);
+            this.textSymbols.Name = "textSymbols";
+            this.textSymbols.Size = new System.Drawing.Size(431, 20);
+            this.textSymbols.TabIndex = 2;
+            this.textSymbols.TextChanged += new System.EventHandler(this.textSymbols_TextChanged);
+            // 
+            // labelAddress
+            // 
+            this.labelAddress.AutoSize = true;
+            this.labelAddress.Location = new System.Drawing.Point(12, 15);
+            this.labelAddress.Name = "labelAddress";
+            this.labelAddress.Size = new System.Drawing.Size(122, 13);
+            this.labelAddress.TabIndex = 3;
+            this.labelAddress.Text = "Стартовая директория";
+            // 
+            // labelFileName
+            // 
+            this.labelFileName.AutoSize = true;
+            this.labelFileName.Location = new System.Drawing.Point(70, 41);
+            this.labelFileName.Name = "labelFileName";
+            this.labelFileName.Size = new System.Drawing.Size(64, 13);
+            this.labelFileName.TabIndex = 4;
+            this.labelFileName.Text = "Имя файла";
+            // 
+            // labelSymbols
+            // 
+            this.labelSymbols.AutoSize = true;
+            this.labelSymbols.Location = new System.Drawing.Point(36, 67);
+            this.labelSymbols.Name = "labelSymbols";
+            this.labelSymbols.Size = new System.Drawing.Size(98, 13);
+            this.labelSymbols.TabIndex = 5;
+            this.labelSymbols.Text = "Символы в файле";
+            // 
+            // buttonChangeAddress
+            // 
+            this.buttonChangeAddress.Location = new System.Drawing.Point(577, 10);
+            this.buttonChangeAddress.Name = "buttonChangeAddress";
+            this.buttonChangeAddress.Size = new System.Drawing.Size(75, 23);
+            this.buttonChangeAddress.TabIndex = 6;
+            this.buttonChangeAddress.Text = "Изменить";
+            this.buttonChangeAddress.UseVisualStyleBackColor = true;
+            this.buttonChangeAddress.Click += new System.EventHandler(this.buttonChangeAddress_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(577, 180);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 70);
+            this.buttonSearch.TabIndex = 7;
+            this.buttonSearch.Text = "Искать";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // labelFound
+            // 
+            this.labelFound.AutoSize = true;
+            this.labelFound.Location = new System.Drawing.Point(12, 458);
+            this.labelFound.Name = "labelFound";
+            this.labelFound.Size = new System.Drawing.Size(211, 13);
+            this.labelFound.TabIndex = 9;
+            this.labelFound.Text = "Файлов обработано: ; Файлов найдено:";
+            // 
+            // labelCurrentFile
+            // 
+            this.labelCurrentFile.AutoSize = true;
+            this.labelCurrentFile.Location = new System.Drawing.Point(12, 487);
+            this.labelCurrentFile.Name = "labelCurrentFile";
+            this.labelCurrentFile.Size = new System.Drawing.Size(87, 13);
+            this.labelCurrentFile.TabIndex = 10;
+            this.labelCurrentFile.Text = "Текущий файл: ";
+            // 
+            // treeViewFiles
+            // 
+            this.treeViewFiles.Location = new System.Drawing.Point(15, 104);
+            this.treeViewFiles.Name = "treeViewFiles";
+            this.treeViewFiles.Size = new System.Drawing.Size(556, 338);
+            this.treeViewFiles.TabIndex = 11;
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(337, 458);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(138, 13);
+            this.labelTime.TabIndex = 12;
+            this.labelTime.Text = "Время с запуска поиска: ";
+            // 
+            // buttonPause
+            // 
+            this.buttonPause.Location = new System.Drawing.Point(577, 271);
+            this.buttonPause.Name = "buttonPause";
+            this.buttonPause.Size = new System.Drawing.Size(75, 70);
+            this.buttonPause.TabIndex = 13;
+            this.buttonPause.Text = "Пауза";
+            this.buttonPause.UseVisualStyleBackColor = true;
+            this.buttonPause.Click += new System.EventHandler(this.buttonPause_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 514);
+            this.Controls.Add(this.buttonPause);
+            this.Controls.Add(this.labelTime);
+            this.Controls.Add(this.treeViewFiles);
+            this.Controls.Add(this.labelCurrentFile);
+            this.Controls.Add(this.labelFound);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonChangeAddress);
+            this.Controls.Add(this.labelSymbols);
+            this.Controls.Add(this.labelFileName);
+            this.Controls.Add(this.labelAddress);
+            this.Controls.Add(this.textSymbols);
+            this.Controls.Add(this.textFileName);
+            this.Controls.Add(this.textAddress);
+            this.Name = "MainForm";
+            this.Text = "Поиск файлов";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textAddress;
+        private System.Windows.Forms.TextBox textFileName;
+        private System.Windows.Forms.TextBox textSymbols;
+        private System.Windows.Forms.Label labelAddress;
+        private System.Windows.Forms.Label labelFileName;
+        private System.Windows.Forms.Label labelSymbols;
+        private System.Windows.Forms.Button buttonChangeAddress;
+        private System.Windows.Forms.FolderBrowserDialog folderAddress;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Label labelFound;
+        private System.Windows.Forms.Label labelCurrentFile;
+        private System.Windows.Forms.TreeView treeViewFiles;
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Button buttonPause;
+    }
+}
+
---
+++ FileFinder/Form1.cs
@@ -0,0 +1,251 @@
+﻿using Microsoft.VisualStudio.Workspace;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileFinder
+{
+    public partial class MainForm : Form
+    {
+        private System.Windows.Forms.Timer aTimer;
+        DateTime timerStart;
+        TimeSpan timerElapsed;
+        
+        private bool loading;
+        PauseTokenSource myPause;
+        private bool paused;
+        private bool cancelled;
+        CancellationTokenSource myCancel;
+
+        public int fileAdded;
+        public int fileChecked;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            loading = false;
+            myPause = new PauseTokenSource();
+            myCancel = new CancellationTokenSource();
+            EnableForm(true);
+            cancelled = true;
+            if(File.Exists("savingproperties.dat"))
+            {
+                LoadData();
+            }
+        }
+
+        public void SaveData()
+        {
+            if (!loading)
+            {
+                using (FileStream output = File.Create("savingproperties.dat"))
+                {
+                    using (BinaryWriter writer = new BinaryWriter(output))
+                    {
+                        writer.Write(textAddress.Text);
+                        writer.Write(textFileName.Text);
+                        writer.Write(textSymbols.Text);
+                    }
+                }
+            }
+        }
+
+        public void LoadData()
+        {
+            loading = true;
+            using (FileStream input = File.OpenRead("savingproperties.dat"))
+            {
+                using (BinaryReader reader = new BinaryReader(input))
+                {
+                    textAddress.Text = reader.ReadString();
+                    textFileName.Text = reader.ReadString();
+                    textSymbols.Text = reader.ReadString();
+                }
+            }
+            loading = false;
+        }
+
+        private async void FindFiles()
+        {
+            EnableForm(false);
+            SetTimer();
+            paused = false;
+            cancelled = false;
+            myCancel = new CancellationTokenSource();
+            treeViewFiles.Nodes.Clear();
+            fileAdded = 0;
+            fileChecked = 0;
+            string address = textAddress.Text;
+            string fileName = textFileName.Text;
+            string symbols = textSymbols.Text;
+
+            var progressFileName = new Progress<string>(s => labelFound.Text = s);
+            var progressCurrentFile = new Progress<string>(s => labelCurrentFile.Text = s);
+            var progressPopulate = new Progress<string>(s => PopulateTreeView(s, '\\'));
+
+            await Task.Run(() =>
+            {
+                CheckFile(address, fileName, symbols, progressFileName, progressCurrentFile, progressPopulate, myPause.Token, myCancel.Token).Wait();
+            });
+
+            aTimer.Stop();
+            EnableForm(true);
+            paused = false;
+            cancelled = true;
+            buttonSearch.Text = "Искать";
+            buttonPause.Text = "Пауза";
+            myCancel.Dispose();
+        }
+
+        private void EnableForm(bool check)
+        {
+            textFileName.Enabled = check;
+            textSymbols.Enabled = check;
+            buttonChangeAddress.Enabled = check;
+            paused = check;
+            buttonPause.Enabled = !check;
+        }
+
+        private async Task CheckFile(string address, string fileName, string symbols, IProgress<string> progressFileName, IProgress<string> progressCurFile, IProgress<string> progressPopulate, PauseToken pauseToken, CancellationToken cancelToken)
+        {
+            foreach (var oneFile in Directory.GetFiles(address, "*", SearchOption.AllDirectories))
+            {
+                await pauseToken.WaitWhilePausedAsync();
+                if (oneFile != null)
+                {
+                    progressCurFile.Report("Текущий файл: " + oneFile);
+                    fileChecked++;
+                    if (Path.GetFileName(oneFile).Contains(fileName) && File.ReadAllText(oneFile).Contains(symbols))
+                    {
+                        progressPopulate.Report(oneFile);
+                        fileAdded++;
+                    }
+                    progressFileName.Report("Файлов обработано:" + fileChecked + "; Файлов найдено: " + fileAdded);
+                }
+                if(cancelToken.IsCancellationRequested)
+                {
+                    break;
+                }
+            }
+        }
+
+        private void textAddress_TextChanged(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+
+        private void textFileName_TextChanged(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+
+        private void textSymbols_TextChanged(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+
+        private void buttonChangeAddress_Click(object sender, EventArgs e)
+        {
+            folderAddress.SelectedPath = textAddress.Text;
+            DialogResult result = folderAddress.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                textAddress.Text = folderAddress.SelectedPath;
+            }
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            if (cancelled)
+            {
+                buttonSearch.Text = "Стоп";
+                cancelled = false;
+                FindFiles();
+            }
+            else
+            {
+                buttonSearch.Text = "Искать";
+                myCancel.Cancel();
+                buttonPause.PerformClick();
+                cancelled = true;
+            }
+        }
+
+        private void PopulateTreeView(string path, char pathSeparator)
+        {
+            TreeNode lastNode = null;
+            string subPathAg = string.Empty;
+            foreach(string subPath in path.Split(pathSeparator))
+            {
+                subPathAg += subPath + pathSeparator;
+                TreeNode[] nodes = treeViewFiles.Nodes.Find(subPathAg, true);
+                if(nodes.Length == 0)
+                {
+                    if(lastNode == null)
+                    {
+                        lastNode = treeViewFiles.Nodes.Add(subPathAg, subPath);
+                    }
+                    else
+                    {
+                        lastNode = lastNode.Nodes.Add(subPathAg, subPath);
+                    }
+                }
+                else
+                {
+                    lastNode = nodes[0];
+                }
+            }
+        }
+
+        void SetTimer()
+        {
+            aTimer = new System.Windows.Forms.Timer();
+            aTimer.Interval = 1;
+            aTimer.Tick += new EventHandler(OnTimerTick);
+            aTimer.Enabled = true;
+            timerStart = DateTime.Now;
+            timerElapsed = TimeSpan.Zero;
+            aTimer.Start();
+        }
+
+        void OnTimerTick(object sender, EventArgs e)
+        {
+            TimeSpan currentElapsed = DateTime.Now - timerStart;
+            TimeSpan elapsed = currentElapsed + timerElapsed;
+            labelTime.Text = "Время с запуска поиска: " + elapsed.ToString(@"hh\:mm\:ss\.fff");
+        }
+
+        void OnTimerPause()
+        {
+            aTimer.Stop();
+            TimeSpan currentElapsed = DateTime.Now - timerStart;
+            timerElapsed += currentElapsed;
+        }
+
+        void OnTimerResume()
+        {
+            timerStart = DateTime.Now;
+            aTimer.Start();
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            if (!paused)
+            {
+                buttonPause.Text = "Возобновить";
+                myPause.Pause();
+                paused = true;
+                OnTimerPause();
+            }
+            else
+            {
+                buttonPause.Text = "Пауза";
+                myPause.Resume();
+                paused = false;
+                OnTimerResume();
+            }
+        }
+    }
+}
